d9848410cf508cc32287f20994e77fb1086d9fd8
Felix Wenhao Wang <31264160+Felixwhwang@users.noreply.github.com>
2017-12-11 22:41:35 -0800

    update

---
+++ src/components/App.vue
@@ -0,0 +1,124 @@
+<template >
+  <div id="app" >
+    <div id="menu">
+       <br>
+
+        <div class="container ">
+            <div v-if="!this.$store.state.user.displayName">
+            <router-link to="/">Home</router-link>
+          <!--<a v-if="!this.$store.state.user.displayName" href="/register" @click="register">Register</a>-->
+            <router-link to="/register">Register</router-link>
+            <router-link to="/login">Login</router-link>
+      <!-- Insert cool photo -->
+          </div>
+        </div>
+    </div>
+    <h1><a href="/login" style="text-decoration:none;"> {{ title }}</a></h1>
+    <img src="https://user-images.githubusercontent.com/31264160/31743405-e7d712cc-b40f-11e7-8c22-d7d9b7b54c43.png">
+          <div class="row" v-if="this.$store.state.user.displayName">
+          <a href="/logout" @click="logout">Logout</a>
+          <br>
+        </div>
+
+          <router-view />
+
+      <div class="">
+        <br>
+        <br>
+        <br>
+        <br>
+      </div>
+
+
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  // data:
+  // {
+  //   showContent:true,
+  // },
+  data () {
+    return {
+      title: 'Toro-Net',
+
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    },
+    hpStaus(){
+      // showNav=false;
+      console.log(this.$store);
+
+    },
+  },
+  mounted() {
+    this.$store.dispatch('getUser')
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+}
+
+#menu {
+  background-image: linear-gradient(to bottom ,#3498db 0,#fff);
+  padding: 30px
+}
+
+#menu a {
+  font-size: 30px;
+  text-decoration: none;
+  padding-top: 20px;
+  color: #000;
+  margin-left: 20px;
+}
+
+#menu a:hover {
+  color: #204d74;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+h1 {
+  color: #645f72;
+  font-style: italic;
+  font-size: 55px;
+  font-family: comic sans ms;
+  background: linear-gradient(180deg,#268dd8, #c4d0da);
+      background-clip: border-box;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: #0000;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+a {
+  color: #860038;
+}
+
+.row {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-left: 70px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container col-sm-4 align-items-center">
+
+    <!-- navbar loaded -->
+      <nav-bar> </nav-bar>
+
+        <form action="/auth/local" method="post">
+                      <div class="">
+                         <h2>Log In!</h2>
+                      </div>
+                  <hr>
+                  <br>
+                      <div class="form-group row">
+                          <input class="form-control col-sm-10" type="email" name="email" placeholder="Email" v-model="email">
+                      </div>
+
+                      <div class="form-group row">
+                          <input type="password" class="form-control col-sm-10" id="password" name="password" placeholder="Password" v-model="password">
+                      </div>
+                      <button class="btn btn-success btn-lg" @click="login" type="submit">Login</button>
+                      <a href="/auth/github" class="btn btn-github btn-sm"><i class="fa fa-github"></i> Login with Github</a>
+        </form>
+
+
+
+  </div>
+</template>
+
+<style scoped>
+.btn-github {
+  border: 0;
+  background: #444;
+  color: white;
+}
+.btn-github:hover {
+  background: #2B2B2B;
+  color: white;
+}
+
+.btn-google {
+  border: 0;
+  background: #db4437;
+  color: white;
+}
+
+.btn-google:hover {
+  background: #c90b0b;
+  color: white;
+}
+
+.form-group input {
+  margin-top: 10px;
+  width: 30%;
+  display: inline-block;
+  box-shadow: 10px 10px 10px -10px rgba(11,90,110,1);
+}
+.btn-group-sm > .btn, .btn-sm {
+    padding: 16px;
+    font-size: .875rem;
+    line-height: 1.5;
+    border-radius: .2rem;
+}
+
+.btn-group-lg > .btn, .btn-lg {
+    padding: 10px;
+    font-size: 1.25rem;
+    line-height: 1.5;
+    border-radius: .3rem;
+}
+
+.row {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-left: 70px;
+}
+</style>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      const user = {
+        email: this.email,
+        password: this.password
+      }
+      this.$store.dispatch('login', user)
+    }
+  }
+}
+</script>
